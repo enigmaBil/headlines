04e28bfb3bdd782faa30249e1c6853210da6db08
enigmaBil <emmanueldigital9@gmail.com>
2021-10-20 19:49:09 +0100

    with jinja template

--- headlines.py
+++ headlines.py
@@ -2,12 +2,16 @@ import feedparser
 
 from flask import Flask
 
+from flask import render_template
+
 app = Flask(__name__)
 
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
             'f24': 'https://www.france24.com/fr/rss',
             'cnn': 'http://rss.cnn.com/rss/edition.rss',
-            'fox': 'http://feeds.foxnews.com/foxnews/latest'
+            'fox': 'http://feeds.foxnews.com/foxnews/latest',
+            'ir': 'https://www.inter-reseaux.org/feed/',
+            'nyt': 'https://rss.nytimes.com/services/xml/rss/nyt/Technology.xml'
             }
 
 @app.route("/")
@@ -29,15 +33,18 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 
 def get_news(publication="f24"):
     feed = feedparser.parse(RSS_FEEDS[publication])
-    first_article = feed['entries'][0]
-    return """<html>
-                <body>
-                    <h1>Headlines </h1>
-                    <b>{0}</b> </ br>
-                    <i>{1}</i> </ br>
-                    <p>{2}</p> </ br>
-                </body>
-            </html>""".format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
+    # first_article = feed['entries'][0]
+    return render_template("index.html", articles=feed['entries'])
+
+
+# """<html>
+#                 <body>
+#                     <h1>Headlines </h1>
+#                     <b>{0}</b> </ br>
+#                     <i>{1}</i> </ br>
+#                     <p>{2}</p> </ br>
+#                 </body>
+#             </html>""".format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
     
 
 if __name__ == '__main__' : 
