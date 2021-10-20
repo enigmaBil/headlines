745e7200be0eeacd966e03d1b840625b5b8d1b9e
enigmaBil <emmanueldigital9@gmail.com>
2021-10-20 22:10:17 +0100

    Using GET

--- headlines.py
+++ headlines.py
@@ -4,6 +4,9 @@ from flask import Flask
 
 from flask import render_template
 
+from flask import request
+
+
 app = Flask(__name__)
 
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
@@ -15,37 +18,15 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
             }
 
 @app.route("/")
-@app.route("/<publication>")
-# def bbc():
-#     return get_news('bbc')
-
-# @app.route("/cnn")
-# def cnn():
-#     return get_news('cnn')
-
-# @app.route("/fox")
-# def fox():
-#     return get_news('fox')
-
-# @app.route("/f24")
-# def f24():
-#     return get_news('f24')
-
-def get_news(publication="f24"):
+def get_news():
+    query = request.args.get("publication")
+    if not query or query.lower() not in RSS_FEEDS : 
+        publication = "bbc"
+    else:
+        publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
     # first_article = feed['entries'][0]
     return render_template("index.html", articles=feed['entries'])
-
-
-# """<html>
-#                 <body>
-#                     <h1>Headlines </h1>
-#                     <b>{0}</b> </ br>
-#                     <i>{1}</i> </ br>
-#                     <p>{2}</p> </ br>
-#                 </body>
-#             </html>""".format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
-    
 
 if __name__ == '__main__' : 
     app.run(port=3000, debug=True)
